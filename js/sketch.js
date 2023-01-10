e58927a81e7d8b3f638fb1ad151d412828611b23
B-Rumbl <78166511+B-Rumbl@users.noreply.github.com>
2023-01-10 11:46:30 +0000

    Animated sinewave

--- js/sketch.js
+++ js/sketch.js
@@ -1,18 +1,29 @@
+let x = 0;
+let y = 0;
+let theta = 0;
+let inc = 0.05; // amount to increment the offset each frame
+let offset = 0; //amount offset from beginning of wave
+let amplitude;
+let freq;
+let maxAngle;
+
 function setup() {
+    amplitude = windowHeight * 0.25;
+    freq = windowWidth * 0.25;
+    maxAngle = (windowWidth / freq) * TWO_PI;
     createCanvas(windowWidth, windowHeight);
-    let x = 0;
-    let y = 0;
-    let theta = 0;
-    let amplitude = windowHeight*0.25;
-    let freq = windowWidth*0.25;
-    let maxAngle = (windowWidth/freq) * TWO_PI;
-    while(theta < maxAngle){
-        y = sin(theta) * amplitude;
-        ellipse(x, y  + height*0.5, 16);
-        theta += 0.2;
-        x = (theta/maxAngle) * windowWidth;
-    }
+    
 }
 function draw() {
-
+    background(0);
+    // noStroke();
+    fill(255, 0, 255);
+    while (theta < maxAngle + offset) {
+        y = sin(theta) * amplitude;
+        circle(x, y + height * 0.5, 500);
+        theta += 0.2;
+        x = ((theta-offset) / maxAngle) * windowWidth;
+    }
+    offset += inc
+    theta = offset;
 }
