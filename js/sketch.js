e37759e148e8552f21a2d1146e3ffd87498e1eee
B-Rumbl <78166511+B-Rumbl@users.noreply.github.com>
2023-01-10 14:26:33 +0000

    Bouncing Circles

--- js/sketch.js
+++ js/sketch.js
@@ -1,40 +1,63 @@
 let x = 0;
 let y = 0;
 let theta = 0;
-let inc = 0.05; // amount to increment the offset each frame
+let inc = 0.008; // amount to increment the offset each frame
 let offset = 0; //amount offset from beginning of wave
 let amplitude;
 let freq;
 let maxAngle;
 
 function setup() {
-    amplitude = windowHeight * 0.25;
-    freq = windowWidth * 0.25;
-    maxAngle = (windowWidth / freq) * TWO_PI;
+    amplitude = windowHeight * 0.5;
+    freq = windowWidth * 0.5;
+    maxAngle = (windowWidth / freq) * PI;
     createCanvas(windowWidth, windowHeight);
-    
+
 }
 function draw() {
-    background(0, 255, 234);
+    background(0);
     noStroke();
-    let length = 10;
+    let length = 50;
     while (theta < maxAngle + offset) {
         //sine wave
         // amplitude = sin(theta - offset) * windowHeight/2
-        amplitude = (theta-offset) / maxAngle * windowHeight // this allows for smaller sine wave at left side of screen
-         for(i = length; i > 0; i--){
+        amplitude = (theta - offset) / maxAngle * (windowHeight * 3)// this allows for smaller sine wave at left side of screen
+
+        //bouncing white dots on the screen
+        // for (i = length; i > 0; i--) {
+        //     rotate(3);
+        //     y = sin(theta - (i * 0.1)) * amplitude;
+        //     fill(255);
+        //     circle(x, y + height * 0.55, 5);
+        // }
+        for (i = length; i > 0; i--) {
+            rotate(3);
+            y = tan(theta - (i * 0.01)) * amplitude;
+            fill(255 - (255 / length * i), 0, 150);
+            circle(x, y + width * 0.55, (50 + i));
+            rotate(5);
+        }
+        for (z = length; z > 0; z--) {
+            rotate(8);
             y = sin(theta - (i * 0.1)) * amplitude;
-            fill(255 - (255/length * i), 0, 150);
-            circle(x, y + height * 0.5, 100);
-         }
-         for(i = length; i > 0; i--){
-            y = cos(theta - (i * 0.01)) * amplitude;
-            fill(255, 255- (255/length * i), 0);
-            circle(x, y + height * 0.5, 100);
-         }
-        // rotate(2);
+            fill(255, 50, 150 - (255 / length * i));
+            circle(z, y + width * 0.1, (10 + i));
+            // rotate(5);
+        }
+        // for (i = length; i > 0; i--) {
+        //     y = sin(theta - (i * 0.1)) * amplitude;
+        //     fill(0, 255 - (255 / length * i), 50);
+        //     circle(x, y + height * 0.55, 5);
+        
+        // }
+        //  for(i = length; i > 0; i--){
+        //     y = cos(theta - (i * 0.3)) * amplitude;
+        //     fill(255, 255- (255/length * i), 0);
+        //     circle(x, y + height * 0.89, 100);
+        //  }
+
         theta += 0.2;
-        x = ((theta-offset) / maxAngle) * windowWidth;
+        x = ((theta - offset) / maxAngle) * windowWidth;
     }
     offset += inc
     theta = offset;
