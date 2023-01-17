b6b471f249ba7f93243aad159290f96f43a084c2
B-Rumbl <78166511+B-Rumbl@users.noreply.github.com>
2023-01-17 15:20:48 +0000

    grid final

--- js/gridtwo.js
+++ js/gridtwo.js
@@ -13,21 +13,26 @@ function preload() {
 
 function setup() {
     colors = [color('#B31583'), color('#FFDC1F'), color('#FF06B5'), color('#1FFBFF'), color('#0CB0B3')]
-    createCanvas(1200, 1200);
-    // background(155);
-    const CELL_SIZE = 60;
+    createCanvas(windowWidth - (50), windowHeight - (50));
+    background(random(colors));
+    const CELL_SIZE = 20;
     let x = 0;
     let y = 0;
-    noStroke();
+        noStroke();
     while (y < height) {
         while (x < width) {
             tint(random(colors));
             let j = random(tex);
+            // rotate(PI * 0.5)
             image(j, x, y, CELL_SIZE);
-            // rotate(random(90))
+            // rotate(windowWidth, windowHeight);
             x += CELL_SIZE;
         }
+        // rotate(random)
         y += CELL_SIZE;
         x = 0;
     }
+  
+}
+function draw() {
 }
