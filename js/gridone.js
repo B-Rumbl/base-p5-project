506423d49837bd23f45e6a4c5df4df814c10eb16
B-Rumbl <78166511+B-Rumbl@users.noreply.github.com>
2023-01-17 13:56:56 +0000

    update

--- js/gridone.js
+++ js/gridone.js
@@ -1,8 +1,8 @@
 function setup() {
-    const CELL_SIZE = 233;
-    createCanvas(1200,1200);
-    background(125);
-    noStroke();
+    const CELL_SIZE = 120;
+    createCanvas(1200, 1200);
+    // background(125);
+    // noStroke();
     let x = 0;
     let y = 0;
     let xVar = 5;
@@ -10,27 +10,27 @@ function setup() {
     let wVar = 13;
     let hVar = 21;
     let sizes = [CELL_SIZE, CELL_SIZE * 2, CELL_SIZE * 3];
-    let curH = random(sizes)
-    let curW = random(sizes)
-    let col = [color('#B31583'), color ('#FFDC1F'), color('#FF06B5'), color('#1FFBFF'), color('#0CB0B3')];
-    while(y < height) {
-        while(x < width){
-            fill(random(col))
+    let curH = random(sizes);
+    let curW = random(sizes);
+    let col = [color('#B31583'), color('#FFDC1F'), color('#FF06B5'), color('#1FFBFF'), color('#0CB0B3')];
+    strokeWeight(5);
+    while (y < height) {
+        while (x < width) {
+            fill(random(col));
             rect(x, y, curW, curH);
-            x+= curW;
+            x += curW;
             let availW = width - x;
             curW = random(sizes);
-            while (curW > availW){
-                curW -= CELL_SIZE
+            while (curW > availW) {
+                curW -= CELL_SIZE;
             }
         }
-        y+= curH;
+        y += curH;
         let availH = height - y;
-        curH = random(sizes)
-        while (curH > availH){
+        curH = random(sizes);
+        while (curH > availH) {
             curH -= CELL_SIZE;
         }
         x = 0;
-        y = 0;
     }
 }
--- js/gridtwo.js
+++ js/gridtwo.js
@@ -1,30 +1,33 @@
 let tex = [];
+let colors;
 
 
-function preload(){
+function preload() {
     let texNum = 5;
-    for(let i = 1; i <= texNum; i++){
+    for (let i = 1; i <= texNum; i++) {
         // let texture = loadImage("images/texture00"+i+".png");//concatenation
         let j = loadImage(`images/texture00${i}.png`); //backticks are 'template literals' and '$' '{}' is string interpolation
         tex.push(j);//adds texture to textures array
     }
 }
 
-function setup(){
-    createCanvas(1200,1200);
-    background(155);
-    const CELL_SIZE =  180;
+function setup() {
+    colors = [color('#B31583'), color('#FFDC1F'), color('#FF06B5'), color('#1FFBFF'), color('#0CB0B3')]
+    createCanvas(1200, 1200);
+    // background(155);
+    const CELL_SIZE = 60;
     let x = 0;
     let y = 0;
     noStroke();
-    while(y < height){
-        while(x < width){
-            tint(random(255));
+    while (y < height) {
+        while (x < width) {
+            tint(random(colors));
             let j = random(tex);
             image(j, x, y, CELL_SIZE);
-            x+=CELL_SIZE;
+            // rotate(random(90))
+            x += CELL_SIZE;
         }
-        y+=CELL_SIZE;
-        x=0;
+        y += CELL_SIZE;
+        x = 0;
     }
 }
