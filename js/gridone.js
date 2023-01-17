4cb010407777f5135fb02a6ba5e245b48a391c99
B-Rumbl <78166511+B-Rumbl@users.noreply.github.com>
2023-01-17 10:46:08 +0000

    update gridone

--- js/gridone.js
+++ js/gridone.js
@@ -9,14 +9,18 @@ function setup() {
     let yVar = 8;
     let wVar = 13;
     let hVar = 21;
+    let sizes = [CELL_SIZE, CELL_SIZE 8 2, CELL_SIZE * 3];
+    let curH = random(sizes)
+    let curW = random(sizes)
     let col = [color('#B31583'), color ('#FFDC1F'), color('#FF06B5'), color('#1FFBFF'), color('#0CB0B3')]
     while(y < height) {
         while(x < width){
             fill(random(col))
-            rect(x+random(-xVar, xVar), y + random(-yVar, yVar), CELL_SIZE + random (-wVar, wVar), CELL_SIZE + random(-hVar, hVar));
-            x+= CELL_SIZE;
+            // rect(x+random(-xVar, xVar), y + random(-yVar, yVar), CELL_SIZE + random (-wVar, wVar), CELL_SIZE + random(-hVar, hVar));
+            rect(x, y, curW, curH);
+            x+= curW;
         }
-        y+= CELL_SIZE;
+        y+= curH;
         x = 0;
     }
 }
